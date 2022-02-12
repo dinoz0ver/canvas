11ba7ef850d6edff421c88fc921b8c8f0ef2e843
Vadim Pilyugin <v.pilyugin@group-ib.com>
2022-02-12 20:01:00 +0300

    адекватный recording

--- simplest1.js
+++ simplest1.js
@@ -3,10 +3,12 @@ var HEIGHT;
 var SIZE;
 var MULTIPLIER;
 
-SIZE  = 32;
+SIZE  = 256;
 WIDTH = SIZE;
 HEIGHT = SIZE;
-MULTIPLIER = 16;
+MULTIPLIER = 2;
+
+var FPS = 25;
 
 let width = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
 let height = window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight;
@@ -26,48 +28,70 @@ RECORDING_CTX.imageSmoothingEnabled = false;
 var CANVAS_CTX = CANVAS.getContext("2d");
 var IMAGE_DATA = CANVAS_CTX.createImageData(WIDTH, HEIGHT);
 
-let videoStream = RECORDING_CANVAS.captureStream(25);
-
-var RECORDER = new MediaRecorder(videoStream);
-
-let chunks = [];
-RECORDER.ondataavailable = function(e) {
-  console.log("recorded data");
-  chunks.push(e.data);
-};
-
-RECORDER.onerror = function(e) {
-  console.log("Recorder error:", e);
-}
-
-var RECORDING_STARTED = false;
-RECORDER.onstart = function(e) {
-  RECORDING_STARTED = true;
-  // RECORDING_CTX.drawImage(CANVAS, 0, 0, WIDTH, HEIGHT, 0, 0, WIDTH*MULTIPLIER, HEIGHT*MULTIPLIER);
-  console.log("Recording started");
-}
-
-RECORDER.onstop = function(e) {
-  if (!RECORDING_STARTED) {
-    console.log("Recording was not started");
-    return;
-  }
-  console.log("recording ended, ", chunks.length, "chunks");
-  RECORDING_STARTED = false;
-  let blob = new Blob(chunks, {
-    type: 'video/webm'
-  });
-  let url = URL.createObjectURL(blob);
-  let a = document.createElement('a');
-  document.body.appendChild(a);
-  a.style = 'display: none';
-  a.href = url;
-  a.download = 'test.webm';
-  a.click();
-  window.URL.revokeObjectURL(url);
-  chunks = [];
-};
-
 function div(a,b) {
     return Math.floor(a/b);
 }
+
+
+// // Promise based timer
+// function wait(ms) {
+//   return new Promise(res => setTimeout(res, ms));
+// }
+// // implements a sub-optimal monkey-patch for requestPostAnimationFrame
+// // see https://stackoverflow.com/a/57549862/3702797 for details
+// // if (!window.requestPostAnimationFrame) {
+// //   window.requestPostAnimationFrame = function monkey(fn) {
+// //     const channel = new MessageChannel();
+// //     channel.port2.onmessage = evt => fn(evt.data);
+// //     requestAnimationFrame((t) => channel.port1.postMessage(t));
+// //   };
+// // }
+// // Promisifies EventTarget.addEventListener
+// function waitForEvent(target, type) {
+//   return new Promise((res) => target.addEventListener(type, res, {
+//     once: true
+//   }));
+// }
+
+
+// let videoStream = RECORDING_CANVAS.captureStream(0);
+// const TRACK = videoStream.getVideoTracks()[0];
+// // Firefox still uses a non-standard CanvasCaptureMediaStream
+// // instead of CanvasCaptureMediaStreamTrack
+// if (!TRACK.requestFrame) {
+//   TRACK.requestFrame = () => videoStream.requestFrame();
+// }
+
+// var RECORDER = new MediaRecorder(videoStream);
+
+// let chunks = [];
+// RECORDER.ondataavailable = function(e) {
+//   console.log("recorded data");
+//   chunks.push(e.data);
+// };
+
+// RECORDER.onerror = function(e) {
+//   console.log("Recorder error:", e);
+// }
+
+// RECORDER.onstart = function(e) {
+//   // RECORDING_CTX.drawImage(CANVAS, 0, 0, WIDTH, HEIGHT, 0, 0, WIDTH*MULTIPLIER, HEIGHT*MULTIPLIER);
+//   console.log("Recording started");
+//   RECORDER.pause();
+// }
+
+// RECORDER.onstop = function(e) {
+//   console.log("recording ended, ", chunks.length, "chunks");
+//   let blob = new Blob(chunks, {
+//     type: 'video/webm'
+//   });
+//   let url = URL.createObjectURL(blob);
+//   let a = document.createElement('a');
+//   document.body.appendChild(a);
+//   a.style = 'display: none';
+//   a.href = url;
+//   a.download = 'test.webm';
+//   a.click();
+//   window.URL.revokeObjectURL(url);
+//   chunks = [];
+// };
