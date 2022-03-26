82044905f455cd90a1f5aa4d8fe67f9290b04cd9
dinoz0ver <susli4ekd@gmail.com>
2022-03-26 13:56:36 +0300

    mariosprite test

--- to.py
+++ to.py
@@ -1,7 +1,11 @@
 from PIL import Image
 import sys
 
-img=Image.open(sys.argv[1]).convert("RGBA")
+s = input("Введите имя файла: ")
+
+img=Image.open(s).convert("RGBA")
 data = map(lambda x: list(x), img.getdata())
 print(list(data))
 print(img.size)
+
+input("Нажмите любую кнопку чтобы закончить программу")
