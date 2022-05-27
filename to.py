e04b34af1cb72a541eeba517edb011e2ee4da8ec
dinoz0ver <susli4ekd@gmail.com>
2022-05-27 23:34:38 +0300

    added new sprites and teams(in progress)

--- to.py
+++ to.py
@@ -1,14 +1,13 @@
 from PIL import Image
 import sys
+while True:
+	s = input("Введите имя файла: ")
 
-s = input("Введите имя файла: ")
-
-img=Image.open(s).convert("RGBA")
-data = map(lambda x: list(x), img.getdata())
-with open(f"{s}.js", "w") as f:
-	f.write(f"var NAME={list(data)};")
-	f.write(f"var NAME_SIZE={img.size};")
-# print(list(data))
-# print(img.size)
-
+	img=Image.open(s).convert("RGBA")
+	data = map(lambda x: list(x), img.getdata())
+	name=s.split(".")[0]
+	with open(f"assets/{name}.js", "w") as f:
+		f.write(f"var {name.upper()}={list(data)};\n")
+		f.write(f"var {name.upper()}_SIZE={{width:{img.size[0]}, height:{img.size[1]}}};")
+	print("Завершено")
 input("Нажмите любую кнопку чтобы закончить программу")
